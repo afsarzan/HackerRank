d1d44dc9c93e18a1e494c7f61c0cd086cec0bce9
Afsar Ahmed <afsar_zan@yahoo.com>
2026-02-12 19:11:51 -0800

    6Q solve

---
+++ 6_maxMinSum.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    arr = arr.sort((a,b) => a-b);
+    const res = arr.reduce( (acc,item) => item + acc,0);
+    console.log(`${res-arr[arr.length-1]} ${res-arr[0]}`)
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
